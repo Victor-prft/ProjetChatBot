1406b024c1d12c9d3fb3cb7c2a331beda56dbf66
Victor <victor.proffit@efrei.net>
2023-11-14 10:40:00 +0100

    venvvv

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from math import *
 from function import *
 
-print(nom_president())
+def idf(repertoire):
+    dictionnaire = {}
+    for fichier in repertoire:
+        for mot in fichier:
+            if mot in dico.keys:
+
+            else:
+                dictionnaire[mot] = 1
