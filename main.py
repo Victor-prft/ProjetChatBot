55975c7b7288f14d21520ba6c10d91c6b8d355c2
Arthur <arthur.boutelier@gmail.com>
2023-11-23 19:26:06 +0100

    fonction qui_a_ecrit et a_parler terminé

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ transformation_fichier("./Speeches")
 matrice = creation_tf_idf("./Cleaned")
 dico_idf = idf("./Cleaned")
 liste_cle_dico = correspondance_mot(dico_idf)
-
+print(a_parler("./Cleaned", "nation"))
