74c908a4b859a63431532f0de8e41ab6d16ad8fd
Victor <victor.proffit@efrei.net>
2023-11-25 13:57:12 +0100

    Commentaire Victor : moins_important et correspondance_mot

--- function.py
+++ function.py
@@ -193,6 +193,13 @@ def indice_tab(tab, element):
 
   
 def moins_important(matrice, correspondance_ligne):
+    """ Cette fonction sert a trouver le ou les mots moins importants de la matrice. C'est mot sont dient moins
+        important si la valeur dans le tableau est égal à 0
+        Entrée : Matrice qui est une liste de liste comportant la note tf-idf. Correspondance_ligne est la liste de mots
+                matrice : list
+                correspondance_ligne : list
+        Sortie : Cette fonction ressort une liste des mots les moins importants de la matrice (tf-idf = 0).
+                liste_moins_important : list"""
     liste_moins_important = []
     for indice_ligne in range(len(matrice)):
         i = 0
--- main.py
+++ main.py
@@ -4,4 +4,6 @@ matrice = creation_tf_idf("./Cleaned")
 dico_idf = idf("./Cleaned")
 liste_cle_dico = correspondance_mot(dico_idf)
 liste_moins_important = moins_important(matrice,liste_cle_dico)
-print(plus_eleve(matrice,liste_cle_dico))
+l = []
+print(type(l))
+
