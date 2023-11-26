ec4d20832661484b38fbaa701c89c873c517a12b
arthu <arthur.boutelier@gmail.com>
2023-11-26 20:32:48 +0100

    début interface

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from function import *
 transformation_fichier("./Speeches")
-matrice = creation_tf_idf("./Cleaned")
-dico_idf = idf("./Cleaned")
-liste_cle_dico = correspondance_mot(dico_idf)
-liste_moins_important = moins_important(matrice,liste_cle_dico)
-
-print(idf("./Cleaned"))
+running = True
+demande = """ Veuillez choisir une option:
+-1: Afficher les mots les moins important
+-2: Afficher les mots avec le plus grand score tf-idf
+-3: Afficher le/les mot(s) les plus utilisé par un président
+-4: Afficher le président qui a le plus dit un mot
+-5: Afficher tout les présidents qui ont dit un mot
+-6: Afficher le premier président ayant parlé de certains termes
+-7: Afficher tout les mots à la fois important et cité par tout les 
+-8: Sortir
+"""
+while running:
+    choix = int(input(demande))
+    if choix == 1:
+        print(moins_important(matrice, correspondance_ligne))
